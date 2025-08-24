45f2a53b14586e1a8045ec8bacd03870b963612f
tkdgh7063 <hoo412298@gmail.com>
2025-08-24 19:55:38 +0900

    Add blog post content page with dynamic route
    - Added GraphQL query to fetch full MDX blog post content
    - Updated blog post page to display the complete content when a post is clicked
    - Introduced dynamic file naming as {mdx.frontmatter__slug}.tsx to generate pages per blog post
    - Ensures readers can view the entire article, not just titles or excerpts

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,36 @@
+import { graphql } from "gatsby";
 import React from "react";
 import Layout from "../../components/Layout";
+import Seo from "../../components/Seo";
 
-export default function BlogPost() {
+interface BlogPostProps {
+  data: Queries.PostDetailQuery;
+  children: any;
+}
+
+export default function BlogPost({ data, children }: BlogPostProps) {
   return (
     <Layout title="Blog Post">
-      <div />
+      <div>{children}</div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      frontmatter {
+        title
+        slug
+        date
+        category
+        authur
+      }
+      body
+    }
+  }
+`;
+
+export const Head = ({ data }: BlogPostProps) => (
+  <Seo title={data.mdx?.frontmatter?.title!} />
+);
